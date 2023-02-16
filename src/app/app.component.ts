bf190fe36845773238ea6c1a0eac3852e6f25a6c
Francisco Gubbins Corral <franciscogubbins@gmail.com>
2023-02-16 17:39:40 +0100

    starting angular project and initializing ui5

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PAR-frontend';
+  
+  public numbers = [0, 1, 2, 3];
+
+}
---
+++ src/app/imports-ui5.ts
@@ -0,0 +1,107 @@
+import "@ui5/webcomponents-fiori/dist/Assets.js";
+import "@ui5/webcomponents-fiori/dist/Assets-static";
+import "@ui5/webcomponents/dist/Assets.js";
+import "@ui5/webcomponents-theming/dist/Assets.js";
+import "@ui5/webcomponents-fiori/dist/features/CoPilotAnimation.js";
+import "@ui5/webcomponents-icons/dist/AllIcons.js";
+import "@ui5/webcomponents-icons-tnt/dist/AllIcons.js";
+import "@ui5/webcomponents-icons-business-suite/dist/AllIcons.js";
+
+import "@ui5/webcomponents/dist/Avatar.js";
+import "@ui5/webcomponents/dist/Avatar";
+import "@ui5/webcomponents/dist/Badge";
+import "@ui5/webcomponents/dist/Breadcrumbs";
+import "@ui5/webcomponents/dist/BreadcrumbsItem";
+import "@ui5/webcomponents/dist/BusyIndicator";
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/Calendar";
+import "@ui5/webcomponents/dist/CalendarDate";
+import "@ui5/webcomponents/dist/CalendarHeader";
+import "@ui5/webcomponents/dist/CalendarPart";
+import "@ui5/webcomponents/dist/Card";
+import "@ui5/webcomponents/dist/CardHeader";
+import "@ui5/webcomponents/dist/Carousel";
+import "@ui5/webcomponents/dist/CheckBox";
+import "@ui5/webcomponents/dist/ColorPalette";
+import "@ui5/webcomponents/dist/ColorPaletteItem";
+import "@ui5/webcomponents/dist/ColorPalettePopover";
+import "@ui5/webcomponents/dist/ColorPicker";
+import "@ui5/webcomponents/dist/CustomListItem";
+import "@ui5/webcomponents/dist/DateComponentBase";
+import "@ui5/webcomponents/dist/DatePicker";
+import "@ui5/webcomponents/dist/DateRangePicker";
+import "@ui5/webcomponents/dist/DateTimePicker";
+import "@ui5/webcomponents/dist/DayPicker";
+import "@ui5/webcomponents/dist/Dialog";
+import "@ui5/webcomponents/dist/FileUploader";
+import "@ui5/webcomponents/dist/Filters";
+import "@ui5/webcomponents/dist/GroupHeaderListItem";
+import "@ui5/webcomponents/dist/Icon";
+import "@ui5/webcomponents/dist/Input";
+import "@ui5/webcomponents/dist/Interfaces";
+import "@ui5/webcomponents/dist/Label";
+import "@ui5/webcomponents/dist/Link";
+import "@ui5/webcomponents/dist/List";
+import "@ui5/webcomponents/dist/ListItem";
+import "@ui5/webcomponents/dist/ListItemBase";
+import "@ui5/webcomponents/dist/Menu";
+import "@ui5/webcomponents/dist/MenuItem";
+import "@ui5/webcomponents/dist/MessageStrip";
+import "@ui5/webcomponents/dist/MonthPicker";
+import "@ui5/webcomponents/dist/Panel";
+import "@ui5/webcomponents/dist/Popover";
+import "@ui5/webcomponents/dist/Popup";
+import "@ui5/webcomponents/dist/ProgressIndicator";
+import "@ui5/webcomponents/dist/RangeSlider";
+import "@ui5/webcomponents/dist/ResponsivePopover";
+import "@ui5/webcomponents/dist/Slider";
+import "@ui5/webcomponents/dist/SliderBase";
+import "@ui5/webcomponents/dist/SplitButton";
+import "@ui5/webcomponents/dist/StandardListItem";
+import "@ui5/webcomponents/dist/StepInput";
+import "@ui5/webcomponents/dist/SuggestionGroupItem";
+import "@ui5/webcomponents/dist/SuggestionItem";
+import "@ui5/webcomponents/dist/SuggestionListItem";
+import "@ui5/webcomponents/dist/Switch";
+import "@ui5/webcomponents/dist/Table";
+import "@ui5/webcomponents/dist/TableCell";
+import "@ui5/webcomponents/dist/TableColumn";
+import "@ui5/webcomponents/dist/TableGroupRow";
+import "@ui5/webcomponents/dist/TableRow";
+import "@ui5/webcomponents/dist/TimePickerBase";
+import "@ui5/webcomponents/dist/TimePicker";
+import "@ui5/webcomponents/dist/TimeSelection";
+import "@ui5/webcomponents/dist/Title";
+import "@ui5/webcomponents/dist/WheelSlider";
+import "@ui5/webcomponents/dist/YearPicker";
+
+
+import "@ui5/webcomponents-fiori/dist/Bar.js";
+import "@ui5/webcomponents-fiori/dist/DynamicSideContent";
+import "@ui5/webcomponents-fiori/dist/FilterItem";
+import "@ui5/webcomponents-fiori/dist/FilterItemOption";
+import "@ui5/webcomponents-fiori/dist/IllustratedMessage";
+import "@ui5/webcomponents-fiori/dist/Interfaces";
+import "@ui5/webcomponents-fiori/dist/Page";
+import "@ui5/webcomponents-fiori/dist/ProductSwitch";
+import "@ui5/webcomponents-fiori/dist/ProductSwitchItem";
+import "@ui5/webcomponents-fiori/dist/SortItem";
+import "@ui5/webcomponents-fiori/dist/Timeline";
+import "@ui5/webcomponents-fiori/dist/TimelineItem";
+import "@ui5/webcomponents-fiori/dist/UploadCollection";
+import "@ui5/webcomponents-fiori/dist/UploadCollectionItem";
+import "@ui5/webcomponents-fiori/dist/ViewSettingsDialog";
+import "@ui5/webcomponents-fiori/dist/ShellBar";
+
+
+
+
+
+
+
+
+
+
+
+
+
